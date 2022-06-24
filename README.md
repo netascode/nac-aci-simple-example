9c97ed664191d6c2ef0a993a033c3b5ff4741fe1
Rob van der Kind <r.vdkind@hotmail.com>
2022-06-24 10:21:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nexus-as-Code - Simple example for ACI
+
+Use Terraform to operate and manage ACI infrastructure using purpose built modules.
+
+## Documentation
+
+Documentation is available ([here](https://insert.pubhub.link)).
---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aci = {
+      source  = "CiscoDevNet/aci"
+      version = ">= 2.1.0"
+    }
+    utils = {
+      source  = "netascode/utils"
+      version = ">= 0.1.1"
+    }
+  }
+}
+
+provider "aci" {
+  username    = "username"
+  password    = "password"
+  url         = "https://apic.url"
+  insecure    = true
+  retries     = 4
+}
+
+locals {
+  model = yamldecode(data.utils_yaml_merge.model.output)
+}
+
+data "utils_yaml_merge" "model" {
+  input = concat([for file in fileset(path.module, "data/*.yaml") : file(file)], [file("${path.module}/defaults/defaults.yaml"), file("${path.module}/modules/modules.yaml")])
+}
+
+module "tenant" {
+  source = "git::https://github.com/netascode/terraform-aci-nac-tenant.git"
+
+  for_each    = toset([for tenant in lookup(local.model.apic, "tenants", {}) : tenant.name])
+  model       = local.model
+  tenant_name = each.value
+}
