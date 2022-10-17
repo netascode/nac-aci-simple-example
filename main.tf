d2e468458f53a4aa6845b26ffd1d1e0950872306
danischm <danischm@cisco.com>
2022-10-17 18:42:05 +0200

    Bump module version

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "utils_yaml_merge" "model" {
 
 module "tenant" {
   source  = "netascode/nac-tenant/aci"
-  version = "0.3.0"
+  version = "0.3.1"
 
   for_each    = toset([for tenant in lookup(local.model.apic, "tenants", {}) : tenant.name])
   model       = local.model
